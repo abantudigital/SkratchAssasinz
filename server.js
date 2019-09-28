c21544c152eb9ae1e2510c920e0bcf534c24377e
Tony <tony.onserio@gmail.com>
2019-09-28 16:13:14 -0500

    Initial commit

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("API RUNNING!"));
+app.get("/", (req, res) => res.send("DJ DOUGIE DOUG!"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log("Server started on port 5000"));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
